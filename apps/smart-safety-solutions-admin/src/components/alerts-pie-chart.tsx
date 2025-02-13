1ce9d9080b4d5a300a3cee7203fd93d7c451b0fe
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2025-02-13 00:56:25 -0800

    updates pie chart loading

--- apps/smart-safety-solutions-admin/src/components/alerts-pie-chart.tsx
+++ apps/smart-safety-solutions-admin/src/components/alerts-pie-chart.tsx
@@ -38,11 +38,15 @@ const chartConfig = {
 };
 
 const AlertsPieChart = () => {
-  const { data: attachedAlarmsCount = 0 } = useFetchAlarmsCountQuery({
-    typeList: [AlarmType.ATTACHED],
-    startTs: thirtyDaysAgo,
-  });
-  const { data: unattachedAlarmsCount = 0 } = useFetchAlarmsCountQuery({
+  const { data: attachedAlarmsCount = 0, isLoading: isAttachedAlarmsLoading } =
+    useFetchAlarmsCountQuery({
+      typeList: [AlarmType.ATTACHED],
+      startTs: thirtyDaysAgo,
+    });
+  const {
+    data: unattachedAlarmsCount = 0,
+    isLoading: isUnattachedAlarmsLoading,
+  } = useFetchAlarmsCountQuery({
     typeList: [AlarmType.UNATTACHED],
     startTs: thirtyDaysAgo,
   });
@@ -66,6 +70,8 @@ const AlertsPieChart = () => {
     (attachedAlarmsCount / totalAlarmsCount) * 100
   );
 
+  const isLoading = isAttachedAlarmsLoading || isUnattachedAlarmsLoading;
+
   return (
     <Card className="flex flex-col border-none shadow-none min-w-[360px]">
       <CardHeader className="items-center pb-0 w-full">
@@ -77,42 +83,46 @@ const AlertsPieChart = () => {
           config={chartConfig}
           className="mx-auto aspect-square max-h-[250px]"
         >
-          <PieChart>
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
-            />
-            <Pie
-              data={chartData}
-              dataKey="count"
-              nameKey="alarmType"
-              innerRadius={60}
-              strokeWidth={5}
-            >
-              <Label
-                content={({ viewBox }) => {
-                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                    return (
-                      <text
-                        x={viewBox.cx}
-                        y={viewBox.cy}
-                        textAnchor="middle"
-                        dominantBaseline="middle"
-                      >
-                        <tspan
+          {!isLoading ? (
+            <PieChart>
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Pie
+                data={chartData}
+                dataKey="count"
+                nameKey="alarmType"
+                innerRadius={60}
+                strokeWidth={5}
+              >
+                <Label
+                  content={({ viewBox }) => {
+                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                      return (
+                        <text
                           x={viewBox.cx}
                           y={viewBox.cy}
-                          className="fill-foreground text-3xl font-bold"
+                          textAnchor="middle"
+                          dominantBaseline="middle"
                         >
-                          {attachedPercentage}%
-                        </tspan>
-                      </text>
-                    );
-                  }
-                }}
-              />
-            </Pie>
-          </PieChart>
+                          <tspan
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            className="fill-foreground text-3xl font-bold"
+                          >
+                            {attachedPercentage}%
+                          </tspan>
+                        </text>
+                      );
+                    }
+                  }}
+                />
+              </Pie>
+            </PieChart>
+          ) : (
+            <div />
+          )}
         </ChartContainer>
       </CardContent>
     </Card>
